f26c7b8ba06cefbd5600b1ed4eeac21bcf757b8e
Victor S. Miller <victorsmiller@gmail.com>
2025-09-30 10:43:41 -0700

    Changed to use mpmath instead of numpy

--- kempner/mollify.py
+++ kempner/mollify.py
@@ -7,73 +7,37 @@ import numpy as np
 import mpmath as mp
 import scipy.integrate
 
-FLOAT = np.float64 | mp.mpf
-COMPLEX = np.complex64 | mp.mpc
+def bump_function(arg: mp.mpf) -> mp.mpf:
 
-FUN = Callable[[FLOAT], FLOAT]
-
-def bump_function(arg: np.float64) -> np.float64:
-
-    return np.exp(-1/(1 - arg ** 2)) if np.abs(arg) < 1 else 0.0
+    return mp.exp(-1/(1 - arg ** 2)) if mp.fabs(arg) < 1 else mp.mpf(0)
 
 @cache
-def normalization() -> np.float64:
+def normalization() -> mp.mpf:
 
-    return 1 / scipy.integrate.quad(bump_function, -1, 1)[0]
+    return 1 / mp.quadgl(bump_function, [-1, 1])
 
-# Note that bump_function is even, so all Fourier Coefficients
+# Note that the bump_function is even, so all Fourier Coefficients
 # Are real.
-def moll_fourier_coeff(arg: np.float64) -> np.float64:
-    "Use scipy.integrate.quad"
+def moll_fourier_coeff(arg: mp.mpf) -> mp.mpf:
+    """
+      Fourier transform of the normalized bump function.
+      Since it's even it's
+
+      2 * integral[0, infty] f(y) cos(2 pi x y) dy
+
+      Since f is normalized the fourier transform at 0 is 1.
+    """
+    if arg == 0:
+        return mp.mpf(1)
     mult = normalization()
-    return mult * scipy.integrate.quad(bump_function,
-                                       -1, 1, weight='cos',
-                                       wvar = - 2 * np.pi * arg)[0]
+    cbump = lambda _: mult * bump_function(_) * mp.cos(2 * mp.pi * arg * _)
+    # re_part = 2 * mp.quadosc(cbump, [0, mp.inf], period = 1 / arg)
+    re_part = 2 * mp.quadgl(cbump, [0,1])
+    # re_part =  mult * scipy.integrate.quad(bump_function,
+    #     -1, 1, weight='cos',
+    #     wvar = - 2 * np.pi * arg)[0]
+    # im_part =  mult * scipy.integrate.quad(bump_function,
+    #     -1, 1, weight='sin',
+    #     wvar = - 2 * np.pi * arg)[0]
+    return re_part
 
-class Bump:
-
-    def __init__(self, use_numpy = True):
-
-        self._use_numpy = use_numpy
-        self._base = np if use_numpy else mp
-        self._exp = np.exp if use_numpy else mp.exp
-        self._pi = np.pi if use_numpy else mp.pi
-        self._abs = np.abs if use_numpy else abs
-        self._quad = (lambda a, b, c: scipy.integrate.quad(a,b,c, complex_func = True)[0]
-            if use_numpy else mp.quad(a, [b, c]))
-        self._total = self._quad(self.unbump, -np.inf, np.inf).real
-
-    def unbump(self, arg: FLOAT) -> FLOAT:
-        " The function exp(-1/(1-x^2)) when |x| < 1, 0 otherwise."
-        
-        return (0.0 if self._abs(arg) >= 1
-                else self._exp(-1/(1 - arg ** 2)))
-
-    def bump(self, arg: FLOAT) -> FLOAT:
-
-        return self.unbump(arg) / self._total
-
-    def bumpb(self, scale: FLOAT) -> Callable[[FLOAT], FLOAT]:
-
-        return lambda _: (1 / scale) * self.bump(_ / scale)
-
-    def afourier(self, scale: FLOAT, num: int) -> Callable[[FLOAT], COMPLEX]:
-        
-        return (lambda _: self.bumpb(scale)(_) *
-                self._exp(-2j * self._pi * num * _))
-    
-    def fourier(self, fun: Callable[[FLOAT], COMPLEX], num: int) -> COMPLEX:
-
-        return (mp.quad(
-            lambda _: fun(_) * self._exp(-2j * self._pi * num * _),
-            -1, 1) if not self._use_numpy
-                else fcoeff(fun, num))
-def prime_zeta_values(limit: int) -> List[FLOAT]:
-
-    main = mp.log(11) / mp.log(10)
-    return [mp.primezeta(main - (2j * mp.pi * _) / mp.log(10))
-            for _ in range(limit)]
-    
-                    
-
-        
