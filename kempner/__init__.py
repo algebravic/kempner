d1c8198b719f2c2c8e855833b9406e1fe111be41
Victor S. Miller <victorsmiller@gmail.com>
2025-09-30 10:46:02 -0700

    defined exposed functions

--- kempner/__init__.py
+++ kempner/__init__.py
@@ -1,2 +1,9 @@
 from .baillie import Baillie
 from .basic import Basic
+from .expand import approximation
+from .best import covers
+__all__ = ['Baillie',
+           'Basic',
+           'approximation',
+           'covers',
+           ]
