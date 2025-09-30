752fe31d1a54d12552ef28aa4c62367a3c8c0f79
Victor S. Miller <victorsmiller@gmail.com>
2025-09-30 10:43:16 -0700

    Used vectorization for the mpmath version

--- kempner/fourier.py
+++ kempner/fourier.py
@@ -14,13 +14,16 @@ class Fourier:
 
         self._base = np if use_numpy else mp
         self._pi = np.pi if use_numpy else mp.pi
-        self._sinc = np.sinc if use_numpy else mp.sincpi
-        self._exp = np.exp if use_numpy else mp.exp
-        self._log = np.log if use_numpy else mp.log
+        self._sinc = np.sinc if use_numpy else np.vectorize(mp.sincpi)
+        self._exp = np.exp if use_numpy else np.vectorize(mp.exp)
+        self._log = np.log if use_numpy else np.vectorize(mp.log)
+        self._mpf = np.float64 if use_numpy else mp.mpf
+        self._mpc = np.complex64 if use_numpy else mp.mpc
+        self._real = np.vectorize(lambda _: _.real)
 
-    def scaled_sinc(self, start: FLOAT, end: FLOAT) -> Callable[[FLOAT],
+    def coefficients(self, start: FLOAT, end: FLOAT) -> Callable[[FLOAT],
                                                                 COMPLEX]:
-        """
+        r"""
         if start < end < start + 1, we produce the fourier transform
         of the periodization mod 1 of the characteristic function, I,  of the
         interval [start, end].
@@ -30,22 +33,41 @@ class Fourier:
           
           Note that np.sinc = sin(pi*x)/(pi * x)
           but mp.sinc = sin(x) / x
-          """
+        """
         if not ((start < end) and (end < start + 1)):
             raise ValueError(f"({start}, {end}) invalid interval")
 
         dif = (end - start)
-        avg = 0.5 * (end + start)
+        pls = (end + start)
 
-        return lambda _: (dif *
-                          self._exp(2j * self._pi * avg * _)
-                          * self._sinc(dif * _))
-    def interval_sinc(self, arg: int, eps: FLOAT) -> Callable[[FLOAT], COMPLEX]:
+        return np.vectorize(lambda _:
+                            (dif * self._exp(- 1j * self._pi * pls * _)
+                             * self._sinc(dif * _)))
 
-        start = self._log(arg) / self._log(10)
-        end = self._log(arg + 1) / self._log(10)
+    def interval_coeffs(self, arg: int,
+                        base: int = 10, eps: FLOAT = mp.mpf(0)) -> Callable[[FLOAT], COMPLEX]:
+        r"""
+           Calculate fourier coefficients for the interval
+           [log(a)/log(b) + epsilon, log(a+1)/log(b) - epsilon]
+        """
 
-        return self.scaled_sinc(start + eps, end - eps)
-    
-        
-        
+        start = self._log(arg) / self._log(base)
+        end = self._log(arg + 1) / self._log(base)
+
+        return self.coefficients(start + eps, end - eps)
+
+    def approx(self, start: FLOAT, end: FLOAT, prec: int, scale: int) -> np.ndarray:
+        r"""
+           return the sum of prec fourier coefficients for the interval
+           function, using scale points in [0,1].
+
+           g[k] = k-th fourier coefficient
+           value[l] =  sum[k](exp(2 * pi * 1j * k * (1/l)) * g[k])
+        """
+        # Calculate evaluation points: equal spaced on [0,1]
+        points = np.arange(scale) / self._mpf(scale)
+        # The Fourier coefficient function
+        func = self.coefficients(start, end)
+        return  self._real(2 * self._exp(2 * self._pi * 1j
+                                         * np.outer(points, np.arange(1, prec)))
+                           @ func(np.arange(1, prec)) + func(0))
