d508c0d58ec6a91387c665ca4a7d28cb6142396b
Victor S. Miller <victorsmiller@gmail.com>
2025-11-17 12:37:48 -0800

    Moved source tree underneath src

--- kempner/__init__.py
+++
@@ -1,9 +0,0 @@
-from .baillie import Baillie
-from .basic import Basic
-from .expand import approximation
-from .best import covers
-__all__ = ['Baillie',
-           'Basic',
-           'approximation',
-           'covers',
-           ]
